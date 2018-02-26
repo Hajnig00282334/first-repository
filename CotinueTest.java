0b3408c405048512a34a4992992008da9b7a3e5c
HAJNALKA BUJDOSO - STUDENT <G00282334@gmit.ie>
2018-02-26 11:52:18 +0000

    week 21 java lab

---
+++ CotinueTest.java
@@ -0,0 +1,11 @@
+public class CotinueTest{
+	public static void main(String[] args){
+		for(int count = 1; count <= 10; count++){
+			if(count != 5){
+			System.out.println(count);
+			}
+		}
+	
+	
+	}
+}
